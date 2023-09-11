63b67549c35e1e88cfcca0e17cfc8c81566e7111
Alex Rocha <rocha.amc@gmail.com>
2023-09-11 10:35:02 -0700

    Resolve Promise-3

--- Promise-3/index.js
+++ Promise-3/index.js
@@ -1,7 +1,7 @@
 function greet(name) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (typeof name === 'string') { 
+      if (typeof name === 'string') {
         resolve('hello ' + name);
       } else {
         reject('Greet expects a string!');
@@ -23,12 +23,12 @@ function uppercaser(str) {
 }
 
 
-// Above we have two functions that return promises. 
-// These are greet and uppercaser. 
-// Notice below how we chain these promises together. The 
-// Result of one is then passed to the next. 
-// When chained all of the promises have to resolve for success. 
-// If any of the promises reject then you end in the catch block. 
+// Above we have two functions that return promises.
+// These are greet and uppercaser.
+// Notice below how we chain these promises together. The
+// Result of one is then passed to the next.
+// When chained all of the promises have to resolve for success.
+// If any of the promises reject then you end in the catch block.
 
 greet('Your name') // Returns a Promise
   .then(str => uppercaser(str))  // Upper case the results from greet() Returns a Promise
@@ -36,10 +36,23 @@ greet('Your name') // Returns a Promise
   .catch(err => console.log(err)) // Catches an error
 
 // Challenges: get greet() to fail by passing a non string value
-// What happens? 
+greet(999).then((message) => {
+  console.log(message)
+}).catch((err) => {
+  console.log(err)
+});
+// What happens?
+// The error message is logged to the console.
 
 // Challenge: get uppercaser() to fail by passing a non string value
-// What happens? 
+uppercaser(999).then((message) => {
+  console.log(message)
+}).catch((err) => {
+  console.log(err)
+});
+// What happens?
+// The error message is logged to the console.
 
-// Challenge: Notice there is only a single .catch() at the end. 
+// Challenge: Notice there is only a single .catch() at the end.
 // Explain the behavior?
+// The .catch() at the end catches any errors that occur in the chain of promises.
