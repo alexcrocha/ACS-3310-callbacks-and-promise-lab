afcdc3aae60a3fc5ac63bda2b37ac7d225fb9bf2
Alex Rocha <rocha.amc@gmail.com>
2023-09-11 11:43:25 -0700

    Resolve Promise-6

--- Promise-6/script.js
+++ Promise-6/script.js
@@ -11,23 +11,13 @@ function onsuccess(pos) {
   console.log(`Latitude : ${latitude}`);
   console.log(`Longitude: ${longitude}`);
   console.log(`More or less ${accuracy} meters.`);
-} 
+}
 
 function onerror(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-navigator.geolocation.getCurrentPosition(onsuccess, onerror, options);
-// navigator.geolocation( successCallback, errorCallback, options )
-
-// navigator.geolocation((pos) => {
-//   // 
-// }, (err) => {
-//   // 
-// }, {})
-
-
-// Challenge: Make this work: 
+// Challenge: Make this work:
 
 function getGeolocation(options) {
   return new Promise( (resolve, reject) => {
@@ -35,9 +25,10 @@ function getGeolocation(options) {
   } )
 }
 
-// Like this: 
+// Like this:
 
 getGeolocation(options)
-  .then(pos => { })
-  .catch(err => { })
+  .then(pos => { onsuccess(pos) })
+  .catch(err => { onerror(err) })
 
+// Check it in action by opening the index.html file in your browser and opening the console.
