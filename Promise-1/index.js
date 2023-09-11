e1681f15fe5478f57d4a195d3c711fa927c3aa9b
Alex Rocha <rocha.amc@gmail.com>
2023-09-11 10:19:30 -0700

    Resolve Promise-1

--- Promise-1/index.js
+++ Promise-1/index.js
@@ -1,8 +1,8 @@
 // Make a new Promise
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // reject("--- Oops ---");
     resolve('>>> Success! <<<');
+    reject("--- Oops ---");
   }, 2000);
 });
 
@@ -17,9 +17,11 @@ p.then((message) => {
 // **Problems to solve**
 
 // **1)** What happens when a promise is rejected? Test it by calling `reject()`
-
+// When the promise is rejected we get the error message that we passed to the reject function.
+// The .catch method is called and the error message is logged to the console.
 // **2)** What happens when you call both `resolve` and `reject`? Test this.
-
-// **3)** Does the order matter you call resolve and reject matter? Test this. 
-
+// The first one called is the one that is executed.
+// **3)** Does the order matter you call resolve and reject matter? Test this.
+// Yes.
 // **4)** What happens if you call `resolve` or `reject` more than once? Test this out for yourself.
+// Only the first one called is executed.
