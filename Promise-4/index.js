93266712f53615e552d4a23e4fa0901d0e6b2a63
Alex Rocha <rocha.amc@gmail.com>
2023-09-11 11:11:12 -0700

    Resolve Promise-4

--- Promise-4/index.js
+++ Promise-4/index.js
@@ -1,14 +1,26 @@
+let userInput;
 const food = new Promise((resolve, reject) => {
-  
-})
+  setTimeout(() => {
+    if (userInput === 'pizza') {
+      resolve('You\'re completely right! Pizza is the best!');
+    } else {
+      reject('No. Just no. Pizza is the best.');
+    }
+  }, 1000);
+});
+
+// resolve food here:
 
 
-// resolve food here: 
+// **Challenge 1** Resolve your promise with the name of your favorite food.
+// Do this by calling the `resolve` with an argument.
 
+// **Challenge 2** Resolve the promise `food` with the `.then()` syntax
+// and print your favorite food to the console.
+userInput = 'pizza';
 
-
-// **Challenge 1** Resolve your promise with the name of your favorite food. 
-// Do this by calling the `resolve` with an argument. 
-
-// **Challenge 2** Resolve the promise `food` with the `.then()` syntax 
-// and print your favorite food to the console. 
+food.then((message) => {
+  console.log(message);
+}).catch((err) => {
+  console.log(err);
+});
